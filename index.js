02dbd64593855443bfd8da44544adc06890b0d68
Derrick <derrickbusman@gmail.com>
2019-09-16 18:49:54 -0400

    Web-Browser active, just need to style "list"

--- index.js
+++ index.js
@@ -1,86 +1,82 @@
-const formBox = document.querySelector('#formBox');
-const budgetBox = document.querySelector('#budgetBox');
-const budgetInput = document.querySelector('.budgetInput');
-const budgetSubmit = document.querySelector('.budgetSubmit');
-const expenseBox = document.querySelector('#expenseBox');
-const expenseInput = document.querySelector('.expenseInput');
-const expenseType = document.querySelector('.expenseType');
-const entertainmentType = document.querySelector('.entertainment');
-const foodType = document.querySelector('.food');
-const clothingType = document.querySelector('.clothing');
-const billsType = document.querySelector('.bills');
-const expenseSubmit = document.querySelector('.expenseSubmit');
-const remainingBalance = document.querySelector('.remainingBalance');
-const listBox  = document.querySelector('#listBox');
-
 
 class Budget {
   constructor() {
     this.budget = 0;
-    this.entertainment = 0;
-    this.food = 0;
-    this.clothing = 0;
-    this.bills = 0;
-    // this.amount =0;
-  }
+    this.entertainment = [];
+    this.food = [];
+    this.clothing = [];
+    this.bills = [];
+  };
+
 
   setBudget(myBudget) {
+    if (myBudget <= 0) {
+      alert("submit a 'budget amount'");
+    }
+    else {
       this.budget = myBudget;
-  }
+    };
+  };
 
   purchase(expenseType, expenseInput) {
-    if (expenseType === "entertainment") {
-      this.entertainment += expenseInput;
-      this.budget -= expenseInput;
-    }
-    else if (expenseType === "food") {
-      this.food += expenseInput;
-      this.budget -= expenseInput;
-    }
-    else if (expenseType === "clothing") {
-      this.clothing += expenseInput;
-      this.budget -= expenseInput;
-    }
-    else if (expenseType === "bills") {
-      this.bills += expenseInput;
-      this.budget -= expenseInput;
-    }
+      if (this.budget - expenseInput < 0) {
+        alert("error: overbudget");
+      }
+      else if (expenseInput <= 0) {
+        alert("input an 'expense amount'");
+      }
+      else {
+        this[expenseType].push(expenseInput);
+        this.budget -= expenseInput;
+        this.renderList(expenseType);
+      };
   };
+
+  renderList(type){
+    const list = document.querySelector(`.${type}`);
+    list.innerHTML = this[type].map(expenseCost => `<li>${expenseCost}</li>`);
+  };
+
+
 };
 
 const budget = new Budget();
 
+
+
 function addBudget(e){
-    e.preventDefault();
-    const formData = new FormData(budgetBox);
-    // console.log(formData.get("budgetInput"));
-    let myBudget = formData.get("budgetInput");
-    budget.setBudget(Number(myBudget));
-    console.log(budget);
-    const span = document.querySelector('.budgetAmount');
-    span.innerHTML = budget.budget;
+  e.preventDefault();
+  const budgetBox = document.querySelector('#budgetBox');
+  const formData = new FormData(budgetBox);
+  let myBudget = formData.get("budgetInput");
+  budget.setBudget(Number(myBudget));
+  console.log(budget);
+  const span = document.querySelector('.budgetAmount');
+  span.innerHTML = `budget: $${budget.budget}`;
+
+  document.querySelector('#budgetBox').reset();
 };
 
 function addExpense(e){
-    e.preventDefault();
-    const formData = new FormData(expenseBox);
-    let expenseInputAmount = formData.get("expenseInput")
-    let expenseInputType = formData.get("expenseType")
-    budget.purchase(expenseInputType, Number(expenseInputAmount));
-    console.log(budget);
-    const remainingBalance = document.querySelector('.remainingBalance');
-    remainingBalance.innerHTML = budget.budget;
-    // remainingBalance.innerHTML = budgetInput.value - expenseInput.value;
-}
+  e.preventDefault();
+  const expenseBox = document.querySelector('#expenseBox');
+  const formData = new FormData(expenseBox);
+  let expenseInputAmount = formData.get("expenseInput")
+  let expenseInputType = formData.get("expenseType")
+  budget.purchase(expenseInputType, Number(expenseInputAmount));
+  console.log(budget);
+  const remainingBalance = document.querySelector('.remainingBalance');
+  remainingBalance.innerHTML = `balance: $${budget.budget}`;
 
-budgetBox.addEventListener("submit", addBudget);
+  document.querySelector('#expenseBox').reset();
+};
+
+function addListItem(e){
+  e.preventDefault();
+  const enetertainmentItem = document.createElement('li');
+  entertainmentItem.innerHTML = expenseInput.value;
+};
+
+budgetBox.addEventListener('submit', addBudget);
 expenseBox.addEventListener('submit', addExpense);
-
-
-
-
-
-
-
-
-
+listBox.addEventListener('submit', addListItem);
